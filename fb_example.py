e68565a6fea8a2d9389dbc2de1f3b67e8f80ce14
Kui Tang <kuitang@gmail.com>
2011-06-07 00:20:43 -0400

    fixed old bug in fb

--- fb_example.py
+++ fb_example.py
@@ -4,12 +4,11 @@ import re, sys, webbrowser
 # Nearly trivial Facebook REST API wrapper
 def fb_get_logged_in_requestor():
     auth_url = "https://www.facebook.com/dialog/oauth?client_id=%s&redirect_uri=https://www.facebook.com/connect/login_success.html&response_type=token&scope=ads_management,offline_access"%FB_APP_ID
-    fb_format_str = "https://api.facebook.com/method/%s?access_token=%s&format=json&%s"
     webbrowser.open_new(auth_url)
     url_bar = raw_input("Paste the URL bar contents here: ")
     match = re.search('access_token=([^&]+)', url_bar)
     access_token = match.group(1)
-    print access_token
+    fb_format_str = "https://api.facebook.com/method/%s?access_token="+access_token+"&format=json&%s"
     return make_requestor(lambda *args: fb_format_str%args)
 
 fb_request = fb_get_logged_in_requestor()
